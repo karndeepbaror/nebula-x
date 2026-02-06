e013dccd1e16c3592950e39597770bf157a909fc
KARNDEEP BAROR <166104224+karndeepbaror@users.noreply.github.com>
2026-02-06 10:40:09 +0530

    Add files via upload

---
+++ TraceIntel/helpers/banner.py
@@ -0,0 +1,25 @@
+from lib.colors import *
+
+main_banner = f"""
+{PINK} _______  ______ _______ _______ _______      _____ __   _ _______ _______      
+    |    |_____/ |_____| |       |______        |   | \  |    |    |______ |      
+    |    |    \_ |     | |_____  |______      __|__ |  \_|    |    |______ |_____ 
+                                                                              
+                     - {CYAN}Trace{PURPLE}Intel{WHITE} - 
+{RED}___Tool made with ❤️| By Karndeep Baror | Osint Tool For Track IP Address___{WHITE}
+
+𝕩  LinkedIn: @karndeepbaror
+🚀 Github: github.com/karndeepbaror
+"""
+
+menu_banner = f"""
+{PINK} _______  ______ _______ _______ _______      _____ __   _ _______ _______      
+    |    |_____/ |_____| |       |______        |   | \  |    |    |______ |      
+    |    |    \_ |     | |_____  |______      __|__ |  \_|    |    |______ |_____ 
+                                                                              
+                     - {CYAN}Trace{PURPLE}Intel{WHITE} - 
+{RED}___Tool made with ❤️| By Karndeep Baror | Osint Tool For Track IP Address___{WHITE}
+
+𝕩  LinkedIn: @karndeepbaror
+🚀 Github: github.com/karndeepbaror
+"""
